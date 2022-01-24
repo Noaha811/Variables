9c775e9a484fc500ab0152a53f37bb12c9303b15
Noaha811 <nanderson95@students.mchenry.edu>
2022-01-24 17:53:39 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    //initializing all variables, using examples of what would be appropriate to use in these cases
+    var myByte: Byte = 10
+    var myShort: Short = 999
+    var myInt: Int = -12345678
+    var myLong: Long = 9999999999999
+    var myFloat: Float = 9.7948F
+    var myDouble: Double = 590123.32482934
+    var myBoolean: Boolean = true
+    var myChar: Char = 'a'
+    //printing all the variables
+    println("Byte variable demo: " + myByte)
+    println("Short variable demo: " + myShort)
+    println("Int variable demo: " + myInt)
+    println("Long variable demo: " + myLong)
+    println("Float variable demo: " + myFloat)
+    println("Double variable demo: " + myDouble)
+    println("Boolean variable demo: " + myBoolean)
+    println("Char variable demo: " + myChar)
+}
